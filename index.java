578f6af46f4f532e4ac31dffc7b4dcc6a31b8278
Fafa Rey <reypatlunag@gmail.com>
2020-01-23 14:56:43 +0800

    Changes

--- index.java
+++ index.java
@@ -1,5 +1,5 @@
 class QuickStart {
     public static void main(String[] args) {
-        System.out.println("Hello, World. Login ni");
+        System.out.println("Hello, World. Login v1 nah ni");
     }
 }
